5fce93497410f822938eb1a5263e09e27ed57b96
RustyNova <rusty.nova.jsb@gmail.com>
2022-05-14 14:34:49 +0200

    New connection
    Took 19 minutes

--- model-db-api/src/config/MySQLConnection.ts
+++ model-db-api/src/config/MySQLConnection.ts
@@ -1,73 +1,28 @@
-import mysqlx, {Session} from "mysqlx";
-import {ISchema, ITable} from "mysqlx/lib/interfaces";
 import {getEnvVariables} from "../tools/getEnvVariables";
+import {Sequelize} from "sequelize";
 
-export class MySQLConnection {
-    private static _instance: MySQLConnection;
-    private _session: Promise<Session>;
-    private _schema: Promise<ISchema>;
+function getSequelize() {
+    getEnvVariables();
+    let {MYSQL_HOST, MYSQL_SCHEMA, MYSQL_USER, MYSQL_PASSWORD} = process.env;
+    MYSQL_HOST = MYSQL_HOST || 'localhost';
+    MYSQL_SCHEMA = MYSQL_SCHEMA || 'test';
+    MYSQL_USER = MYSQL_USER || 'root';
+    MYSQL_PASSWORD = MYSQL_PASSWORD || 'root';
 
-    private constructor() {
-        getEnvVariables();
-        this._session = mysqlx.getSession({
-            host: process.env.MYSQL_HOST,
-            user: process.env.MYSQL_USER,
-            password: process.env.MYSQL_PASSWORD,
-            port: 33060,
-        })
+    return new Sequelize(MYSQL_SCHEMA, MYSQL_USER, MYSQL_PASSWORD, {
+        host: MYSQL_HOST,
+        dialect: 'mysql'
+    });
+}
 
-        this._schema = this._session.then(value => {
-            if (!process.env.MYSQL_SCHEMA) {
-                throw new Error("MYSQL_SCHEMA environment variable is not set");
-            }
-            return value.getSchema(process.env.MYSQL_SCHEMA);
-        });
-    }
+const sequelize = getSequelize();
+export default sequelize;
 
-    public static getInstance(): MySQLConnection {
-        if (!MySQLConnection._instance) {
-            MySQLConnection._instance = new MySQLConnection();
-        }
-        return MySQLConnection._instance;
-    }
-
-    public static checkConnection(): Promise<boolean> {
-        return new Promise((resolve) => {
-            getEnvVariables();
-            const connection = mysqlx.getSession({
-                host: process.env.MYSQL_HOST,
-                user: process.env.MYSQL_USER,
-                password: process.env.MYSQL_PASSWORD,
-                port: 33060,
-            });
-            connection.then(() => {
-                resolve(true);
-            }).catch(err => {
-                console.error(err);
-                resolve(false);
-            });
-        });
-    }
-
-    public async getSession(): Promise<Session> {
-        return await this._session;
-    }
-
-    public async getSchema(): Promise<ISchema> {
-        return await this._schema;
-    }
-
-    public async getTable(tableName: string): Promise<ITable> {
-        if (await this.checkIfTableExist(tableName)) {
-            return this._schema.then(schema => schema.getTable(tableName));
-        } else {
-            throw new Error(`Table "${tableName}" does not exist`);
-        }
-    }
-
-    public checkIfTableExist(tableName: string): Promise<boolean> {
-        return this._schema.then(schema => schema.getTable(tableName)).then(async table => {
-            return await table.existsInDatabase();
-        });
+export async function checkDBConnection() {
+    try {
+        await sequelize.authenticate();
+        console.log('Connection has been established successfully.');
+    } catch (error) {
+        console.error('Unable to connect to the database:', error);
     }
 }
