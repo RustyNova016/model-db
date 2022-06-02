6ae9b5629d667cb1253c7de69fb5a8bb8dbf31af
RustyNova <rusty.nova.jsb@gmail.com>
2022-06-02 10:37:56 +0200

    STAPPP
    Took 3 minutes

--- tools/CRUD/SequelizeConnection.ts
+++ tools/CRUD/SequelizeConnection.ts
@@ -1,9 +1,6 @@
-import {getEnvVariables} from "../../model-db-api/src/tools/getEnvVariables";
 import {Sequelize} from "sequelize";
-import logger from "../../model-db-api/src/tools/logger";
 
 export function getSequelizeConnection() {
-    getEnvVariables();
     let {MYSQL_HOST, MYSQL_SCHEMA, MYSQL_USER, MYSQL_PASSWORD} = process.env;
     MYSQL_HOST = MYSQL_HOST || 'localhost';
     MYSQL_SCHEMA = MYSQL_SCHEMA || 'model-db_dev';
@@ -27,8 +24,8 @@ export default sequelize;
 export async function checkDBConnection() {
     try {
         await sequelize.authenticate();
-        logger.info('Connection has been established successfully.', "Database");
+        console.info('Connection has been established successfully.', "Database");
     } catch (error) {
-        logger.error('Unable to connect to the database: \n' + error, "Database");
+        console.error('Unable to connect to the database: \n' + error, "Database");
     }
 }
